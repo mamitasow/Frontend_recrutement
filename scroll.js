38f960130d221912772f5f1098d3ce58a8883f6f
mamitasow <Gayeyayematy@gmail.com>
2019-03-15 13:02:55 +0000

    Scroll done

--- scroll.js
+++ scroll.js
@@ -1,20 +1,29 @@
 $(document).ready(function(){
-    $('#scroll-left').click(function(){
-        var element = $(".first");
-        console.log(element.width())
-        element.animate({marginLeft: -element.width()}, 'fast');
-        element.next().addClass('first');
-        element.removeClass('first')
-        // $('#scrollable').scrollLeft(300);
-        // document.getElementById('scrollable').scrollLeft -= 155;
+    var directions = ['Toutes les directions','DGA','RH','DMGP','DG','DDE','DFC','A','B','C','D','E','F','G'];
+    var first = directions.slice(1,7)    
+    for (var i = 0; i < first.length; i++) {
+        $('#scrollable').append("<span class='filter-item font-weight-bold align-middle w-35 d-block'>"+directions[i]+"</span>")
+    }
+
+    $('#scroll-right').click(function(){  
+        var right = directions.slice(7)
+        $('#scrollable').html('');
+        for (var i = 0; i < right.length; i++) {
+            if (i == 0) {
+                $('#scrollable').append("<span class='filter-item font-weight-bold active align-middle w-35 d-block'>"+right[i]+"</span>")
+            }else{
+            $('#scrollable').append("<span class='filter-item font-weight-bold align-middle w-35 d-block'>"+right[i]+"</span>")
+            }
+        }
     })
 
-    $('#scroll-right').click(function(){
-        var element = $(".last");
-        element.animate({marginRight: -200}, 'fast');
-        element.prev().addClass('last');
-        element.removeClass('last')
-        // document.getElementById('scrollable').scrollLeft -= 155;
+    $('#scroll-left').click(function(){  
+        var left = directions.slice(1,7)
+        $('#scrollable').html('');
+        for (var i = 0; i < left.length; i++) {
+            console.log(left[i])
+            $('#scrollable').append("<span class='filter-item font-weight-bold align-middle w-35 d-block'>"+left[i]+"</span>")
+        }
     })
 
     $('.filter-item').click(function(){
